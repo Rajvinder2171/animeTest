293a6bc156818f2149c813fbec304fb6accc29e1
Rajvinder2171 <79782229+Rajvinder2171@users.noreply.github.com>
2021-10-24 21:24:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ $(window).scroll(function() {
         $('#navBar').removeClass('floatingNav');
     }
 });
+
+
+window.resize(function(){
+   
+     if (screen.width <= 650){
+       
+var iframe = document.getElementById("slider").setAttribute("src", "/slider/index.html");
+    }
+    
+    // else if(screen.width <= 960){
+    //     document.getElementByTagName('iframe').src = 'desktopVideoFeed.html';
+    // }
+})
